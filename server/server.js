9ceacd2835f674c74224b76476eb9c5cb1afb901
Snehal-Salvi <snehalsalvi099@gmail.com>
2024-06-26 13:05:40 +0530

    fixing issues

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import maidRoutes from "./routes/maidRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-import passport from "./utils/passport.js"
-import authRoutes from "./routes/authRoutes.js"
+import passport from "./utils/passport.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -19,23 +19,29 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-  next()
-})
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://667bc483d506d3c28237e931--lucky-sunflower-c1d185.netlify.app"
+  );
+  next();
+});
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
+    origin: [
+      "http://127.0.0.1:3000",
+      "https://667bc483d506d3c28237e931--lucky-sunflower-c1d185.netlify.app",
+    ],
     methods: "GET, POST, PATCH, DELETE, PUT",
     credentials: true,
   })
-)
+);
 
-app.use(cookieParser())
-passport(app)
+app.use(cookieParser());
+passport(app);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Api is running....");
@@ -44,9 +50,8 @@ app.get("/", (req, res) => {
 app.use("/api/maids", maidRoutes);
 app.use("/api/users", userRoutes);
 app.use("/auth", authRoutes);
-app.use("/api/orders", orderRoutes)
+app.use("/api/orders", orderRoutes);
 
- 
 app.use(notFound);
 app.use(errorHandler);
 
